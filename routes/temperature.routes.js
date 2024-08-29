d444487b65874caf47bacfcf9315719d92e193fb
GilinCordero <a01562929@tec.mx>
2024-08-28 18:31:08 -0600

    step3

---
+++ routes/temperature.routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const TemperatureVO = require ('../valueobjects/temperature.vo');
+
+router.post("/v1/temperature/convert", (request, response) => {
+    const input = request.body;
+    console.log(input);
+ 
+    const temperature = new TemperatureVO(20, "CELSIUS");
+
+    response.send(temperature).status(200);
+ });
+
+ module.exports = router;
