259dde5727885b8338a7869133dd26148f2a2110
GilinCordero <a01562929@tec.mx>
2024-08-28 18:22:01 -0600

    v1

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const apiRoutes = require("./routes/temperature.routes");
-
+const apiRoutes = require("./routes/temperature.route");
 // App
 const app = express()
 // Morgan
